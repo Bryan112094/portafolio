fdd359f1b96125e1722d91458e99989cf30a8bce
Cristhian <briian1120@gmail.com>
2023-01-23 17:15:11 -0500

    vamos por sobre mi

--- static/js/main.js
+++ static/js/main.js
@@ -13,4 +13,8 @@
         });
     }
 
+    //Habilitar los tooltip
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 })(jQuery);
