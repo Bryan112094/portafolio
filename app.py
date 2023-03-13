be4257c7b83cc0f14f6ecc75dedfea2c8d335331
Cristhian <briian1120@gmail.com>
2023-03-13 12:53:19 -0500

    completo

--- app.py
+++ app.py
@@ -6,18 +6,6 @@ from flask import(
 
 app = Flask(__name__)
 
-app.config.from_mapping(
-    SECRET_KEY = os.environ.get('SECRET_KEY'),
-    DATABASE_HOST = os.environ.get('FLASK_DATABASE_HOST'),
-    DATABASE_PASSWORD = os.environ.get('FLASK_DATABASE_PASSWORD'),
-    DATABASE_USER = os.environ.get('FLASK_DATABASE_USER'),
-    DATABASE = os.environ.get('FLASK_DATABASE')
-)
-
-# Guardar todas las imagenes
-CARPETA = os.path.join('uploads')
-app.config['CARPETA'] = CARPETA
-
 @app.route('/')
 def index():
     return render_template('index.html')
